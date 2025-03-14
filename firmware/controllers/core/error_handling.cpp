4e048b058bf031133932def0027f41a5b1ef6650
Vichkins888 <vichkins888@gmail.com>
2025-03-14 20:41:35 +0300

    Update error_handling.cpp

--- firmware/controllers/core/error_handling.cpp
+++ firmware/controllers/core/error_handling.cpp
@@ -160,7 +160,6 @@ const char *errorCookieToName(ErrorCookie cookie)
 #define printErrorState()											\
 do {																\
 	PRINT("Power cycle count: %lu", bootCount);						\
-PRINT("TEST");						\
 																	\
 	if (cookie == ErrorCookie::None) {								\
 		break;														\
